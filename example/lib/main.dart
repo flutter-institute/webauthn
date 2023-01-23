cb6c6e0a36da6f93839e671ce733050d954c4a2d
thomastthai <thomas.t.thai@gmail.com>
2023-01-23 08:28:54 -0800

    Fix issue #3
    "Should only be changing the databaseFactory if the platform is Desktop."

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -12,11 +12,11 @@ void main() {
   if (Platform.isWindows || Platform.isLinux) {
     // Initialize FFI
     sqfliteFfiInit();
-  }
 
-  // Change the default factory. On iOS/Android, if not using `sqlite_flutter_lib` you can forget
-  // this step, it will use the sqlite version available on the system.
-  databaseFactory = databaseFactoryFfi;
+    // Change the default factory. On iOS/Android, if not using `sqlite_flutter_lib` you can forget
+    // this step, it will use the sqlite version available on the system.
+    databaseFactory = databaseFactoryFfi;
+  }
 
   runApp(const MyApp());
 }
